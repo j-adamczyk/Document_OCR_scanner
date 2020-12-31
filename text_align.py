3e6603eb4a0ebb3d42ba0c138f2f96c036ae2fea
wolski0420 <wolski0420@gmail.com>
2020-12-31 22:21:40 +0100

    Refactor

--- text_align.py
+++ text_align.py
@@ -1,49 +1,43 @@
-from skimage.io import imread, imsave
-from skimage.filters import gaussian, threshold_otsu
-from skimage.color import rgb2gray, rgba2rgb
-from skimage.feature import canny
-from skimage.transform import probabilistic_hough_line, rotate
-from skimage import img_as_ubyte
 import numpy as np
+from skimage.color import rgb2gray
+from skimage.feature import canny
+from skimage.restoration import denoise_bilateral
+from skimage.transform import probabilistic_hough_line, rotate
 
 
 def align_text(image: np.ndarray) -> np.ndarray:
     """
-        Rotate sheet to get aligned, parallel text
+    Function which rotates sheet to get aligned, parallel text
 
-        :param image:
-            loaded image as numpy.ndarray type
+    Parameters
+    ----------
+    image : numpy.ndarray
+        photo with skewed text, it must be RGB (not gray or RGBA !!!)
 
-        :return:
-            rotated image as numpy.ndarray type
+    Returns
+    -------
+    numpy.ndarray
+        image with aligned, parallel text
+
     """
 
-    # checking if image is gray, rgba or rgb
-    draft = image
-    if len(draft.shape) > 2:
-        if draft.shape[2] == 4:         # converting from rgba to rgb
-            draft = rgba2rgb(draft)
-        if draft.shape[2] == 3:         # converting from rgb to gray
-            draft = rgb2gray(draft)
+    # rgb -> gray
+    draft = rgb2gray(image)
 
-    # Otsu's method - returns threshold to determine text block
-    thresh: float = threshold_otsu(draft)
+    # filtering image to preserve lines better
+    filtered_img = denoise_bilateral(draft)
 
-    # creating binary image where (True = text) and converting it to gaussian blur
-    binary_img: np.ndarray = draft > thresh
-    gaussian_blur: np.ndarray = gaussian(binary_img, 3)
-
-    # detecting edges and adopting probabilistic line Hough transform
-    edges: np.ndarray = canny(gaussian_blur)
+    # detecting edges and applying probabilistic line Hough transform
+    edges = canny(filtered_img)
     lines = probabilistic_hough_line(edges)
 
     # every hough line needs to be transformed to slope value and then to the angle (like in linear functions)
-    slopes = [(y2 - y1) / (x2 - x1) if (x2 - x1) else 0 for (x1, y1), (x2, y2) in lines]
-    deg_angles = [np.degrees(np.arctan(x)) for x in slopes]
+    slopes = np.array([(y2 - y1) / (x2 - x1) if (x2 - x1) else 0 for (x1, y1), (x2, y2) in lines])
+    deg_angles = np.degrees(np.arctan(slopes))
 
-    # getting most common degree value
-    histogram = np.histogram(deg_angles, bins=180)
-    rotation_angle: float = histogram[1][np.argmax(histogram[0])]
+    # getting the most common degree value
+    hist, angles = np.histogram(deg_angles, bins=180)
+    rotation_angle: float = angles[np.argmax(hist)]
 
     # computing correct rotate angle
     if rotation_angle < -45:                 # -50 -> 40 (we don't want text to lose horizontal/vertical orientation)
@@ -54,10 +48,3 @@ def align_text(image: np.ndarray) -> np.ndarray:
     # rotating and returning image
     img = rotate(image, angle=rotation_angle)
     return img
-
-
-# example
-file_name: str = "skewed.jpg"
-img_file: np.ndarray = imread(file_name)
-rotated_image = align_text(img_file)
-imsave("rotated_" + file_name, img_as_ubyte(rotated_image))
